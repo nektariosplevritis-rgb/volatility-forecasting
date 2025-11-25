f454e2af5cd8e5d05ba162e3e5d7f85b420e5760
nektariosplevritis-rgb <nektarios.plevritis@gmail.com>
2025-11-25 05:11:13 +0200

    Implement evaluate_forecast function
    Add evaluation function for forecasting models

--- src/Evaluate.py
+++ src/Evaluate.py
@@ -1,1 +1,10 @@
+import pandas as pd
 
+def evaluate_forecast(actual, har_pred, lstm_pred):
+    results = {
+        "HAR RMSE": ((actual - har_pred)**2).mean()**0.5,
+        "LSTM RMSE": ((actual[-len(lstm_pred):] - lstm_pred)**2).mean()**0.5,
+        "HAR Directional": ((actual * har_pred > 0).mean()),
+        "LSTM Directional": ((actual[-len(lstm_pred):] * lstm_pred > 0).mean())
+    }
+    return results
